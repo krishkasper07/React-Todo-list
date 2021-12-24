200094d24c2bba77ff4a628f2b166be0a8047abf
kishore kannan <kishorekannan63@gmail.com>
2021-12-24 19:25:05 +0530

    added delete & taskCompleted funtionality

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import AddTodo from "./components/AddTodo/addTodo";
 import './App.css';
+import TodoList from "./components/List/todoList";
 function App() {
   const [todo,setTodo]=useState('');
   const [todos,setTodos]=useState([]);
@@ -14,15 +15,32 @@ function App() {
     }
     let newtodo={
       id:todos.length+1,
-      todo:todo.trim()
+      todo:todo.trim(),
+      isComplete:false
     }
     setTodos([...todos,newtodo]);
     setTodo('');
+  }
+  const handleTaskCompleted=taskId=>{
+     const newTask=[...todos]
+     newTask.forEach(item=>{
+       if(item.id === taskId){
+         item.isComplete=!item.isComplete
+       }
+     })
+     setTodos(newTask);
+     console.log(todos);
+  }
+  const deleteTask=taskId=>{
+   const newTask=todos.filter(todo=>todo.id !==taskId)
+   setTodos(newTask);
   }
   return (
     <div className="App">
       <span>Add Your Todo ✍️</span>
     <AddTodo todo={todo} getTodo={getTodo} addTodos={addTodos}/>
+    <TodoList todos={todos} handleTaskCompleted={handleTaskCompleted} 
+    deleteTask={deleteTask}/>
     </div>
   );
 }
--- src/components/List/todoList.jsx
+++ src/components/List/todoList.jsx
@@ -1,10 +1,33 @@
-import { useState } from "react";
-const TodoList = () => {
-    const [todos,setTodo]=useState([]);
-    
-    return ( <>
-
-    </> );
+import './todoList.css';
+const TodoList = ({todos,handleTaskCompleted,deleteTask}) => {
+    const tasks=todos.map((todo)=>{
+        return <li key={todo.id}>
+            <div style={{display:'flex'}}>
+                <p className={todo.isComplete ? "taskComplete":"taskIncomplete"}>
+                {todo.todo}
+                </p>
+            </div>
+            <div>
+                <button className='completebtn'
+                onClick={()=>handleTaskCompleted(todo.id)}>
+                     completed
+                </button>
+                <button className='editbtn'>
+                    EDIT
+                </button>
+                <button
+                className='deletebtn' 
+                onClick={()=>deleteTask(todo.id)}>
+                     Delete
+                </button>
+            </div>
+        </li>
+    })
+    return (<div className="tasks">
+        <ul>
+           {tasks}
+        </ul>
+    </div>);
 }
  
 export default TodoList;
